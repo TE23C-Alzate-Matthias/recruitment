d044cbb1c985b7686664f7ce0969e795047253f9
Matthias Alzate <matthias.alzate@elev.ga.ntig.se>
2024-10-17 13:15:12 +0200

    ghaerwgergaer

---
+++ recruitment/Program.cs
@@ -0,0 +1,35 @@
+﻿List<string> potential = [
+"Shea Zink",
+"Chantal Beckwith",
+"Lee Wolff",
+"Demonte Royer",
+"Gissel Erickson",
+"Yisroel Huntley",
+"Dania Kitchens",
+"Donovan Mai",
+"Garett Lytle",
+"Kristyn White",
+];
+
+List<string> current = [];
+
+int i;
+
+while (current.Count < 3) 
+{
+    Console.WriteLine("=== Potential Recruits ===");
+
+    for (i = 0; i < potential.Count; i++);
+    {
+        Console.WriteLine($"[{i}] {potential[i]}");
+    }
+
+    Console.WriteLine("=== Current Recruits ===");
+
+    for (i = 0; i < current.Count; i++)
+    {
+        Console.WriteLine($"{current[i]}");
+    }
+
+    
+}
